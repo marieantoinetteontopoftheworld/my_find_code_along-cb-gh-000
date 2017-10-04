93cd580c8cd69f2d5654a493901d432e9d591296
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-04 20:58:28 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0
 
+  meets_condition = false
+  while i < collection.length
+    meets_condition << yield(collection[i])
+
+    if meets_condition
+      return collection[i]
+    i += 1
+  end
 end
